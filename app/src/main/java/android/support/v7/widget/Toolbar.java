086ba739f70aff6e8b1a899a4dde45bf05de73d4
Daniel Jersey <jerseyd@wit.edu>
2016-07-25 13:35:27 -0400

    Adding Files

---
+++ app/src/main/java/android/support/v7/widget/Toolbar.java
@@ -0,0 +1,14 @@
+package android.support.v7.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jerseyd on 7/6/2016.
+ */
+public class Toolbar extends View {
+    public Toolbar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
