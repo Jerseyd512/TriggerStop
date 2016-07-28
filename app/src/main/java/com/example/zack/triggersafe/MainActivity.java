3fef8b7dc88c813c4aee60f8390c55404f16c3a1
Zack Farrer <farrerzack@gmail.com>
2016-07-27 23:28:49 -0400

    -Added bluetooth SPP library

--- app/src/main/java/com/example/zack/triggersafe/MainActivity.java
+++ app/src/main/java/com/example/zack/triggersafe/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        BluetoothSPP bt = new BluetoothSPP(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
